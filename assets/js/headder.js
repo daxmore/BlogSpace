2ef907583463147194c8254468c7045ab855cf0f
daksh <moredaksh19@gmail.com>
2025-02-16 16:25:46 +0530

    mobile view changed in header

--- assets/js/headder.js
+++ assets/js/headder.js
@@ -1,10 +1,19 @@
 const mobileMenuButton = document.getElementById('mobile-menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
-const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
-const mobileMenuClose = document.querySelector('.mobile-menu-close');
+const closeMenuButton = document.getElementById('close-menu');
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const closeIcon = document.getElementById('close-icon');
 
+// Toggle Mobile Menu
 mobileMenuButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
-    mobileMenuIcon.classList.toggle('hidden');
-    mobileMenuClose.classList.toggle('hidden');
+    hamburgerIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+});
+
+// Close menu when clicking outside
+closeMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.add('hidden');
+    hamburgerIcon.classList.remove('hidden');
+    closeIcon.classList.add('hidden');
 });
